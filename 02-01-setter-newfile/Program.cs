f3ce6928ee9c31571285acf294923eb6dda9d6a1
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-30 08:20:13 +0200

    A feladat kiinduló állapota

---
+++ 02-01-setter-newfile/Program.cs
@@ -0,0 +1,24 @@
+﻿// S1.01
+// A feladat szövege:
+// Vektorgrafikus rendszerekben lehetőség van négyzet rajzolására.
+// Az ilyen négyzetek méretét lehetséges változtatni.
+// Az ilyen CAD rendszereknél fontos lehet a négyzet területének és kerületének meghatározása.
+// Készítsen olyan négyzet osztályt amelynek adott az oldalhossza!
+// Az oldalhossz lekérhető és módosítható tulajdonsága legyen a négyzetnek!
+// A négyzet területe és kerülete legyen lekérdezhető tulajdonsága a négyzetnek!
+// Példányosítson egy ilyen négyzetet és a főprogramban mutassa be a tulajdonságok kezelését!
+// Az osztály a projekt model rétegében készítse el egy külön fájlba!
+
+using System;
+
+namespace _02_01_setter_newfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // S1.01 Készítsünk egy model mappát a projektünkben.
+            //       Abba hozzunk létre egy osztályt Square néven.
+        }
+    }
+}
